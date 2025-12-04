c2a61a63252e4df200e21cc4f66954a86f9c5e4b
as <allensnape@gmail.com>
2025-12-05 01:20:40 +0800

    chore: Use NimBLE only, due to BlueDroid is fading out
    And document is formatted by VSCode
    
    https://github.com/espressif/arduino-esp32/issues/11822#issuecomment-3284194493

--- BleKeyboard.h
+++ BleKeyboard.h
@@ -1,30 +1,21 @@
-// uncomment the following line to use NimBLE library
-//#define USE_NIMBLE
-
 #ifndef ESP32_BLE_KEYBOARD_H
 #define ESP32_BLE_KEYBOARD_H
 #include "sdkconfig.h"
 #if defined(CONFIG_BT_ENABLED)
 
-#if defined(USE_NIMBLE)
-
+#include "NimBLEConnInfo.h"
+#include "NimBLEDevice.h"
+#include "NimBLEServer.h"
 #include "NimBLECharacteristic.h"
 #include "NimBLEHIDDevice.h"
 
-#define BLEDevice                  NimBLEDevice
-#define BLEServerCallbacks         NimBLEServerCallbacks
+#define BLEDevice NimBLEDevice
+#define BLEServerCallbacks NimBLEServerCallbacks
 #define BLECharacteristicCallbacks NimBLECharacteristicCallbacks
-#define BLEHIDDevice               NimBLEHIDDevice
-#define BLECharacteristic          NimBLECharacteristic
-#define BLEAdvertising             NimBLEAdvertising
-#define BLEServer                  NimBLEServer
-
-#else
-
-#include "BLEHIDDevice.h"
-#include "BLECharacteristic.h"
-
-#endif // USE_NIMBLE
+#define BLEHIDDevice NimBLEHIDDevice
+#define BLECharacteristic NimBLECharacteristic
+#define BLEAdvertising NimBLEAdvertising
+#define BLEServer NimBLEServer
 
 #include "Print.h"
 
@@ -119,7 +110,6 @@ const MediaKeyReport KEY_MEDIA_WWW_BACK = {0, 32};
 const MediaKeyReport KEY_MEDIA_CONSUMER_CONTROL_CONFIGURATION = {0, 64}; // Media Selection
 const MediaKeyReport KEY_MEDIA_EMAIL_READER = {0, 128};
 
-
 //  Low level key report: up to 6 keys and shift, ctrl etc at once
 typedef struct
 {
@@ -131,30 +121,30 @@ typedef struct
 class BleKeyboard : public Print, public BLEServerCallbacks, public BLECharacteristicCallbacks
 {
 private:
-  BLEHIDDevice* hid;
-  BLECharacteristic* inputKeyboard;
-  BLECharacteristic* outputKeyboard;
-  BLECharacteristic* inputMediaKeys;
-  BLEAdvertising*    advertising;
-  KeyReport          _keyReport;
-  MediaKeyReport     _mediaKeyReport;
-  std::string        deviceName;
-  std::string        deviceManufacturer;
-  uint8_t            batteryLevel;
-  bool               connected = false;
-  uint32_t           _delay_ms = 7;
+  BLEHIDDevice *hid;
+  BLECharacteristic *inputKeyboard;
+  BLECharacteristic *outputKeyboard;
+  BLECharacteristic *inputMediaKeys;
+  BLEAdvertising *advertising;
+  KeyReport _keyReport;
+  MediaKeyReport _mediaKeyReport;
+  std::string deviceName;
+  std::string deviceManufacturer;
+  uint8_t batteryLevel;
+  bool connected = false;
+  uint32_t _delay_ms = 7;
   void delay_ms(uint64_t ms);
 
-  uint16_t vid       = 0x05ac;
-  uint16_t pid       = 0x820a;
-  uint16_t version   = 0x0210;
+  uint16_t vid = 0x05ac;
+  uint16_t pid = 0x820a;
+  uint16_t version = 0x0210;
 
 public:
   BleKeyboard(std::string deviceName = "ESP32 Keyboard", std::string deviceManufacturer = "Espressif", uint8_t batteryLevel = 100);
   void begin(void);
   void end(void);
-  void sendReport(KeyReport* keys);
-  void sendReport(MediaKeyReport* keys);
+  void sendReport(KeyReport *keys);
+  void sendReport(MediaKeyReport *keys);
   size_t press(uint8_t k);
   size_t press(const MediaKeyReport k);
   size_t release(uint8_t k);
@@ -165,18 +155,18 @@ public:
   void releaseAll(void);
   bool isConnected(void);
   void setBatteryLevel(uint8_t level);
-  void setName(std::string deviceName);  
+  void setName(std::string deviceName);
   void setDelay(uint32_t ms);
 
   void set_vendor_id(uint16_t vid);
   void set_product_id(uint16_t pid);
   void set_version(uint16_t version);
-protected:
-  virtual void onStarted(BLEServer *pServer) { };
-  virtual void onConnect(BLEServer* pServer) override;
-  virtual void onDisconnect(BLEServer* pServer) override;
-  virtual void onWrite(BLECharacteristic* me) override;
 
+protected:
+  virtual void onStarted(BLEServer *pServer) {};
+  virtual void onConnect(BLEServer *pServer, NimBLEConnInfo &connInfo) override;
+  virtual void onDisconnect(BLEServer *pServer, NimBLEConnInfo &connInfo, int reason) override;
+  virtual void onWrite(BLECharacteristic *me, NimBLEConnInfo &connInfo) override;
 };
 
 #endif // CONFIG_BT_ENABLED
